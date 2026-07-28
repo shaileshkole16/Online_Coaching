afd21b77ccfa1b53df7f78bf1ffa4ce41a0f27ca
shaileshkole16 <shaileshkole16@gmail.com>
2026-07-28 18:37:51 +0530

    feat: add User entity with authentication fields

---
+++ online-coaching-system/src/main/java/com/coaching/entities/User.java
@@ -0,0 +1,28 @@
+package com.coaching.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer userId;
+
+    private String name;
+    
+    @Column(unique = true)
+    private String email;
+    
+    @JsonIgnore
+    private String password;
+    
+    private String role; // student, teacher, admin
+}
